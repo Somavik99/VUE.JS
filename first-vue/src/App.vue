4876f446e5d91417683531909890f04f44381c3c
Avik Roy <somavik99@gmail.com>
2023-04-16 01:14:15 +0530

    first commit

---
+++ first-vue/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0)
+</script>
+
+<template>
+  <main>
+    <div>
+      <h1>{{ count }}</h1>
+      <button id="inc" @click="count++">+</button>
+      <button id="dec" @click="count--">-</button>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+
+  height: 50vh;
+  width: 50vw;
+  background: rgba(8, 107, 194, 0.548);
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+}
+</style>
