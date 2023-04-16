92d1fed7ad028f10564814ce133b8230c06ab3e2
Avik Roy <94855290+Somavik99@users.noreply.github.com>
2023-04-16 11:39:24 +0530

    Update App.vue

--- first-vue/src/App.vue
+++ first-vue/src/App.vue
@@ -1,15 +1,37 @@
-<script setup>
+<!-- composition API -->
+<!-- <script setup>
 import { ref } from 'vue';
 
 const count = ref(0)
+</script> --> 
+
+<!-- option API -->
+
+<script>
+export default ({
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    AddCount() {
+      this.count += 1
+    },
+    SubtractCount(){
+      this.count -= 1
+    }
+  }
+})
 </script>
+
 
 <template>
   <main>
     <div>
       <h1>{{ count }}</h1>
-      <button id="inc" @click="count++">+</button>
-      <button id="dec" @click="count--">-</button>
+      <button id="inc" @click="SubtractCount()">-</button>
+      <button id="dec" @click="AddCount()">+</button>
     </div>
   </main>
 </template>
